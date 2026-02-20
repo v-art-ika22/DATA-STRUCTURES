cb062d328712dd25871f94c0d4349f05c3f2136f
v-art-ika22 <vartikaagnihotri9402@gmail.com>
2026-02-20 08:12:15 +0530

    do

--- src/LinkedList/first.java
+++ src/LinkedList/first.java
@@ -1,4 +1,35 @@
 package LinkedList;
 
 public class first {
+    class Node{
+        int value;
+        Node next;
+        Node(int data){
+            this.value=data;
+            this.next= null;
+        }
+    }
+    Node head;
+    void insertStart(int value){
+        Node newNode = new Node(value);
+        newNode.next=head;
+        head=newNode;
+    }
+    //print
+    void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value+"->");
+            temp=temp.next;
+        }
+        System.out.print("end");
+    }
+    public static void main(String[]args){
+        first list = new first();
+        list.insertStart(5);
+        list.insertStart(6);
+        list.insertStart(9);
+        list.insertStart(5);
+        list.display();
+    }
 }
